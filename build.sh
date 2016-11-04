293d5985cc8e398c9703bc3bbe144fa5ee13bee5
Wallace Wan <wallace.wan@sap.com>
2016-11-04 17:03:00 +0800

    Support remote deploy

--- build.sh
+++ build.sh
@@ -2,10 +2,19 @@
 
 mvn clean package
 
-docker_img="motan-tenant"
+docker_registry="10.58.9.201:5000"
+docker_name="motan-tenant"
+docker_tag=$(git log --format="%H" -n 1)
+docker_img="$docker_registry/$docker_name:$docker_tag"
 
+cp deploy.sh.bak deploy.sh
+chmod +x deploy.sh
+sed -i 's/${GIT_COMMIT}/'"$docker_tag"'/' deploy.sh
+
+docker ps | grep $docker_img | awk '{print $1}' | xargs docker kill
 docker ps -a | grep $docker_img | awk '{print $1}' | xargs docker rm
-docker rmi $(docker images | grep $docker_img | awk $'{print $3}')
+docker rmi $(docker images | grep $docker_name | grep $docker_tag | awk $'{print $3}')
 
 docker build --force-rm=true --no-cache=true -t $docker_img .
+docker push $docker_img
 #Done
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -d -p 8080:8080 motan-tenant
